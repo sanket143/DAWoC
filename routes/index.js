f4c3c65db3dd9eceb919df7de63d7c220ad7d2ac
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2018-11-30 04:18:20 +0530

    Modified calls to promises

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const request = require('request');
 
-var AWS = require("aws-sdk");
+const AWS = require("aws-sdk");
+
 
 /* WARNING: DynamoDB Config */
 AWS.config.update({
   region: "ap-south-1",
   endpoint: "https://dynamodb.ap-south-1.amazonaws.com"
 });
-var docClient = new AWS.DynamoDB.DocumentClient();
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 
 /* GET home page. */
@@ -17,8 +19,61 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/user/signin/callback', function(req, res, next){
-  console.log(req.query);
+  const code = req.query.code // GitHub Response Client code;
+
+  var getAccessToken = new Promise((resolve, reject) => {
+    request.post(
+      "https://github.com/login/oauth/access_token",
+      {
+        json: {
+          client_id: "14adcfea97df1c43f605",
+          client_secret: process.env.CLIENT_SECRET,
+          code: code
+        }
+      },
+      function(err, res, body){
+        if(!err && res.statusCode == 200){
+          resolve(body.access_token);
+        } else {
+          reject(err);
+        }
+      }
+    );
+  }); // Promise(getAccessToken)
+
+  var getUserData = new Promise((resolve, reject) => {
+    getAccessToken.then((access_token) => {
+      request.get(
+        "https://api.github.com/user?access_token=" + access_token,
+        {
+          headers: {
+            "User-Agent": "Sanket Chaudhari"
+          }
+        },
+        function(err, res, body){
+          if(!err && res.statusCode == 200){
+            resolve(body);
+          } else {
+            reject(err);
+          }
+        }
+      )
+    }).catch((err) => {
+      console.log("Error in getting Access Token:", err)
+    });
+  }); // Promise(getUserData)
+
+  getUserData.then((userData) => {
+    console.log(userData);
+  }).catch((err) => {
+    console.log("Error getting user Data:", err);
+  });
+
   res.redirect("/");
 })
 
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
 module.exports = router;
